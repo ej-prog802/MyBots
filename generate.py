294d283468a5f5808ecaf3a0676ed8550b59abf5
ej-prog802 <ejoconne@uvm.edu>
2021-02-13 10:01:58 -0500

    pyramid

--- generate.py
+++ generate.py
@@ -8,4 +8,8 @@ i = .5
 while(i<10.5):
     pyrosim.Send_Cube(name="Box", pos=[0, 0, i], size=[length, width, height])
     i += 1
+    length = length * 0.9
+    height = height * 0.9
+    width = width * 0.9
+
 pyrosim.End()
