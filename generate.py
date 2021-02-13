b971103d54e09f367733bb79d328c2fb5614acf9
ej-prog802 <ejoconne@uvm.edu>
2021-02-13 10:29:12 -0500

    Genworld

--- generate.py
+++ generate.py
@@ -1,15 +1,9 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
-i = .5
-while(i<10.5):
-    pyrosim.Send_Cube(name="Box", pos=[0, 0, i], size=[length, width, height])
-    i += 1
-    length = length * 0.9
-    height = height * 0.9
-    width = width * 0.9
-
-pyrosim.End()
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    length = 1
+    width = 1
+    height = 1
+    pyrosim.Send_Cube(name="Box", pos=[0, 0, height/2], size=[length, width, height])
+    pyrosim.End()
