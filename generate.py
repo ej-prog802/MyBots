19077772dc30804101272eb842acf30399639648
ej-prog802 <ejoconne@uvm.edu>
2021-02-13 11:13:05 -0500

    Body_errors

--- generate.py
+++ generate.py
@@ -5,5 +5,16 @@ def Create_World():
     length = 1
     width = 1
     height = 1
-    pyrosim.Send_Cube(name="Box", pos=[0, 0, height/2], size=[length, width, height])
+    pyrosim.Send_Cube(name="Box", pos=[0, 10, height/2], size=[length, width, height])
     pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    length = 1
+    width = 1
+    height = 1
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position="0.5 0 1")
+    pyrosim.Send_Cube(name="leg", pos=[1.0, 0, 1.5], size=[length, width, height])
+    pyrosim.End()
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -1,10 +1,11 @@
 import pybullet as p
-import pybullet_data
-import time
+import pybullet_data, time, os
+#os.system("python3 generate.py")
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
+planeId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 for i in range(0, 1500):
     p.stepSimulation()
