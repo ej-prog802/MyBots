b10cb26da4fc2d1e7309ac772375b685c9ec4856
ej-prog802 <ejoconne@uvm.edu>
2021-03-29 19:49:12 -0400

    Hill CLimber Improved

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@ amplitude = numpy.pi/4
 frequency = 75
 phaseOffset = 0
 sz = 2000
-numberOfGenerations = 4
+numberOfGenerations = 6
--- hillclimber.py
+++ hillclimber.py
@@ -10,11 +10,13 @@ class HILL_CLIMBER:
 
     def Evolve(self):
         show = False
-        self.parent.Evaluate(show)
+        self.parent.Evaluate(True)
         for currentGeneration in range(0, c.numberOfGenerations):
-            if currentGeneration == c.numberOfGenerations-1:
-                show = True
+            if currentGeneration == c.numberOfGenerations - 1:
+                # show = True
+                pass
             self.Evolve_For_One_Generation(show)
+        self.Show_Best()
 
     def Evolve_For_One_Generation(self, show):
         self.Spawn()
@@ -34,3 +36,7 @@ class HILL_CLIMBER:
         print("\nP=" + str(self.parent.fitness) + " : C=" + str(self.child.fitness))
         if self.child.fitness <= self.parent.fitness:
             self.parent = self.child
+
+    def Show_Best(self):
+        self.parent.Evaluate(True)
+        print(self.parent.fitness)
